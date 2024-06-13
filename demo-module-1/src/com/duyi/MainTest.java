fa898fa7bb30610f5be343f5b9817a45d2e5432b
LiXiaoYang888 <243596937@qq.com>
2024-06-13 16:27:20 +0800

    这是test提交的冲突

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -3,5 +3,7 @@ package com.duyi;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("....");
+        
+        System.out.println("修改了，测试   test里的");
     }
 }
